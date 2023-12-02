389e2cce64bfcf2fea66d4c7ce8b286642e72f83
Jan-Pieter Cornet <jp@spamhaus.com>
2023-12-02 12:54:29 +0100

    day 1 part 2

--- day1_trebuchet/src/lib.rs
+++ day1_trebuchet/src/lib.rs
@@ -1,15 +1,68 @@
 use exrunner::ExRunner;
 use std::io::BufRead;
+use std::collections::HashMap;
 
 pub fn solve(input: impl BufRead, er: &mut ExRunner) {
+    let numbers = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
     let in_vec: Vec<String> = input.lines()
                     .map(|x| x.unwrap()).collect();
     er.parse_done();
     let in_digits: Vec<Vec<&str>> = in_vec.iter()
         .map(|x| x.matches(char::is_numeric).collect()).collect();
     let in_nums: Vec<i32> = in_digits.iter()
-        .map(|x| format!("{}{}", x[0], x[x.len()-1]).parse().unwrap()).collect();
+        .map(|x| {
+            if x.len() > 0 {
+                format!("{}{}", x[0], x[x.len()-1]).parse().unwrap()
+            } else { 0 }
+        }
+    ).collect();
     er.part1(in_nums.into_iter().sum::<i32>(), Some("Sum of scattered numbers"));
+    let in2_digits: Vec<i32> = in_vec.iter()
+        .map(|l| {
+            let mut lpos = l.find(|c: char| c.is_ascii_digit());
+            let mut lval = None;
+            if let Some(pos) = lpos {
+                let bval = (l[pos..pos+1].as_bytes())[0] - b'0';
+                lval = Some(bval.into());
+            }
+            for (name, val) in &numbers {
+                if let Some(pos) = l.find(*name) {
+                    if lpos.is_none() || pos < lpos.unwrap() {
+                        lpos = Some(pos);
+                        lval = Some(*val);
+                    }
+                }
+            }
+            let mut rpos = l.rfind(|c: char| c.is_ascii_digit());
+            let mut rval = None;
+            if let Some(pos) = rpos {
+                let bval = (l[pos..pos+1].as_bytes())[0] - b'0';
+                rval = Some(bval.into());
+            }
+            for (name, val) in &numbers {
+                if let Some(pos) = l.rfind(*name) {
+                    if rpos.is_none() || pos > rpos.unwrap() {
+                        rpos = Some(pos);
+                        rval = Some(*val);
+                    }
+                }
+            }
+            lval.unwrap_or(0) * 10 + rval.unwrap_or(0)
+        }
+    ).collect();
+    // er.debugln(&format!("in2_digits = {:?}", in2_digits));
+    er.part2(in2_digits.into_iter().sum::<i32>(), None);
 }
 
 #[cfg(test)]
@@ -27,9 +80,30 @@ treb7uchet
         )
     }
 
+    fn test_input2() -> BufReader<&'static [u8]> {
+        BufReader::new(
+            "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+".as_bytes()
+        )
+    }
+
     #[test]
-    fn test_part12() {
+    fn test_part1() {
         let er = ExRunner::run("day 1 - trebuchet".to_string(), solve, test_input());
         er.print_raw();
         assert_eq!(er.answ()[0], Some("142".to_string()));
-    }}
+    }
+
+    #[test]
+    fn test_part2() {
+        let er = ExRunner::run("day 1 - trebuchet".to_string(), solve, test_input2());
+        er.print_raw();
+        assert_eq!(er.answ()[1], Some("281".to_string()));
+    }
+}
