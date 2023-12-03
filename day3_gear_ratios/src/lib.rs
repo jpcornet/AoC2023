6ecaffa3e5523897639bf1eec0450689c428c0a3
Jan-Pieter Cornet <jp@spamhaus.com>
2023-12-04 00:57:10 +0100

    day 3 part 2. TIL that returning a closure with a lot of bound variables causes weird compiler errors. Better to flatten it into a Vec<_> to make sure the closure gets resolved into a result.

--- day3_gear_ratios/src/lib.rs
+++ day3_gear_ratios/src/lib.rs
@@ -1,12 +1,15 @@
 use exrunner::ExRunner;
 use std::io::BufRead;
 use regex::Regex;
+use std::collections::HashMap;
 
 const SYMBOLS: &str = "@#$%^&*-+=<>?/";
 
 pub fn solve(input: impl BufRead, er: &mut ExRunner) {
     let field: Vec<Vec<_>> = input.lines().map(|x| x.unwrap().into_bytes()).collect();
     let numbers_re = Regex::new(r"[0-9]+").unwrap();
+    // keep a hashmap of the numbers found near gears.
+    let mut gears = HashMap::new();
     // look for numeric strings in field, then search around for a symbol
     let sum: i32 = field.iter().enumerate().flat_map(|(y, l)| {
         let nums: Vec<_> = numbers_re.find_iter(String::from_utf8(l.to_vec()).unwrap().as_str())
@@ -24,7 +27,25 @@ pub fn solve(input: impl BufRead, er: &mut ExRunner) {
                     )
                 );
                 if hassymb {
+                    // get any "gear" around this number
+                    let gearpos = (upper..lower+1).into_iter()
+                        .flat_map(|ty| {
+                            (left..right+1).into_iter()
+                                .filter_map(|tx| {
+                                    if field[ty][tx] == b'*' {
+                                        Some((tx, ty))
+                                    } else {
+                                        None
+                                    }
+                                }
+                            ).collect::<Vec<_>>().into_iter()
+                        }
+                    );
                     let res: i32 = m.as_str().parse().unwrap();
+                    for gp in gearpos {
+                        gears.entry(gp).and_modify(|x: &mut Vec<i32>| x.push(res))
+                            .or_insert(vec![res]);
+                    }
                     Some(res)
                 } else {
                     None
@@ -33,6 +54,15 @@ pub fn solve(input: impl BufRead, er: &mut ExRunner) {
         nums.into_iter()
     }).sum();
     er.part1(sum, Some("Sum of numbers with symbols"));
+    let gearsum: i32 = gears.values().filter_map(|gear| {
+        if gear.len() == 2 {
+            // er.debugln(&format!("Got a gear at {} and {}", gear[0], gear[1]));
+            Some(gear[0] * gear[1])
+        } else {
+            None
+        }
+    }).sum();
+    er.part2(gearsum, Some("Sum of gears"));
 }
 
 #[cfg(test)]
