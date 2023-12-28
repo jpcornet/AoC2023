8c6c4a4ff6e3d908f1b62cf8ead356658939caa4
Jan-Pieter Cornet <jp@spamhaus.com>
2023-12-28 17:17:04 +0100

    day 10 part 2.

--- day10_pipe_maze/src/lib.rs
+++ day10_pipe_maze/src/lib.rs
@@ -35,13 +35,13 @@ const SOUTH: DIR = 4;
 const EAST: DIR = 8;
 
 // PIPES is used as the lookup table for character -> pipe directions.
-static PIPES: phf::Map<char, DIR> = phf_map! {
-    '|' => NORTH | SOUTH,
-    '-' => EAST | WEST,
-    'L' => NORTH | EAST,
-    'J' => NORTH | WEST,
-    '7' => SOUTH | WEST,
-    'F' => SOUTH | EAST,
+static PIPES: phf::Map<u8, DIR> = phf_map! {
+    b'|' => NORTH | SOUTH,
+    b'-' => EAST | WEST,
+    b'L' => NORTH | EAST,
+    b'J' => NORTH | WEST,
+    b'7' => SOUTH | WEST,
+    b'F' => SOUTH | EAST,
 };
 
 // mirror a direction
@@ -80,9 +80,9 @@ fn walk_pipe(pm: &Pipemaze, pos: (usize, usize), dir: DIR) -> Option<((usize, us
         return None;
     }
     let newx = (pos.0 as i32 + dx) as usize;
-    let pchar = pm.field[newy][newx] as char;
+    let pchar = pm.field[newy][newx];
     // println!("Walk in direction {dir} from {},{} to {newx},{newy}. Now at {pchar}", pos.0, pos.1);
-    if pchar == 'S' {
+    if pchar == b'S' {
         // We've reached the starting position again
         return Some(((newx, newy), 0 as DIR));
     } else if let Some(pdir) = PIPES.get(&pchar) {
@@ -98,30 +98,74 @@ fn walk_pipe(pm: &Pipemaze, pos: (usize, usize), dir: DIR) -> Option<((usize, us
     return None;
 }
 
-fn walk_around(pm: &Pipemaze, mut dir: DIR) -> Option<usize> {
+// Walk around in the pipemaze, starting at start pos in direction DIR
+// if not possible, returns None. If it is possible, returns the path length
+// until we reach the start pos again.
+// second return value is a copy of the field with only the path itself on it
+fn walk_around(pm: &Pipemaze, startdir: DIR) -> (Option<usize>, Vec<Vec<u8>>) {
     let mut pos = pm.startpos;
     let mut pathlen = 0;
+    let mut dir = startdir;
+    let mut pathonly: Vec<Vec<u8>> = Vec::new();
+    // fill the pathonly with dots, same size as input pipemaze
+    for l in &pm.field {
+        pathonly.push( std::iter::repeat('.' as u8).take(l.len()).collect() );
+    }
     while let Some((newpos, newdir)) = walk_pipe(pm, pos, dir) {
         pathlen += 1;
         pos = newpos;
-        dir = newdir;
         if pos == pm.startpos {
-            return Some(pathlen);
+            // determine the starting point shape size. We started with startdir, and we end with dir into the startpos
+            let startdirs = startdir | mirror_dir(dir);
+            let startshape = PIPES.entries().filter_map(|(&shape, &dirs)| if dirs == startdirs { Some(shape) } else { None }).next().expect("Unknown start directions");
+            pathonly[pos.1][pos.0] = startshape as u8;
+            return (Some(pathlen), pathonly);
+        } else {
+            // copy this element of the path
+            pathonly[pos.1][pos.0] = pm.field[pos.1][pos.0];
+            dir = newdir;
         }
     }
-    None
+    (None, pathonly)
+}
+
+fn count_enclosed(field: &Vec<Vec<u8>>) -> usize {
+    let mut in_path = false;
+    field.iter().map(move |l| {
+        l.iter().filter(move |&&c| {
+            // pretend to scan just south of the "-" marker. Anytime we cross the path, flip the "in-path" indicator.
+            // This means it flips not only on | but also on F and 7. It does not flip on L and J.
+            if c == b'|' || c == b'7' || c == b'F' {
+                in_path = !in_path;
+                false
+            } else {
+                c == b'.' && in_path
+            }
+        }).count()
+    }).sum()
 }
 
 pub fn solve(input: impl BufRead, er: &mut ExRunner) {
     let pm = parse(input);
     er.parse_done();
+    // er.debugln(&format!("Input pipemaze:\n{}", field_to_string(&pm.field)));
     for dir in [NORTH, WEST, SOUTH, EAST] {
-        if let Some(pathlen) = walk_around(&pm, dir) {
+        if let (Some(pathlen), pathonly) = walk_around(&pm, dir) {
             er.part1(pathlen / 2, Some("Halfway of loop length"));
+            // er.debugln(&format!("Path only:\n{}", field_to_string(&pathonly)));
+            let enclosed_tiles = count_enclosed(&pathonly);
+            er.part2(enclosed_tiles, Some("Number of enclosed tiles"));
             break;
         }
     }
 }
+
+// fn field_to_string(field: &Vec<Vec<u8>>) -> String {
+//     let lines: Vec<_> = field.iter().map(|l| {
+//         str::from_utf8(l).unwrap()
+//     }).collect();
+//     lines.join("\n")
+// }
 
 #[cfg(test)]
 mod tests {
@@ -150,17 +194,87 @@ L|-JF
         )
     }
 
-    #[test]
-    fn test_part1() {
-        let er = ExRunner::run("day 10".to_string(), solve, test_input1());
-        er.print_raw();
-        assert_eq!(er.answ()[0], Some("8".to_string()));
+    fn test_input3() -> BufReader<&'static [u8]> {
+        BufReader::new(
+"...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+".as_bytes()
+        )
+    }
+
+    fn test_input4() -> BufReader<&'static [u8]> {
+        BufReader::new(
+".F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+".as_bytes()
+        )
+    }
+
+    fn test_input5() -> BufReader<&'static [u8]> {
+        BufReader::new(
+"FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+".as_bytes()
+        )
     }
 
     #[test]
-    fn test2_part1() {
+    fn test_part12() {
+        let er = ExRunner::run("day 10".to_string(), solve, test_input1());
+        er.print_raw();
+        assert_eq!(er.answ()[0], Some("8".to_string()));
+        assert_eq!(er.answ()[1], Some("1".to_string()));
+    }
+
+    #[test]
+    fn test2_part12() {
         let er = ExRunner::run("day 10".to_string(), solve, test_input2());
         er.print_raw();
         assert_eq!(er.answ()[0], Some("4".to_string()));
+        assert_eq!(er.answ()[1], Some("1".to_string()));
+    }
+
+    #[test]
+    fn test_part2() {
+        let er = ExRunner::run("day 10".to_string(), solve, test_input3());
+        er.print_raw();
+        assert_eq!(er.answ()[1], Some("4".to_string()));
+    }
+
+    #[test]
+    fn test2_part2() {
+        let er = ExRunner::run("day 10".to_string(), solve, test_input4());
+        er.print_raw();
+        assert_eq!(er.answ()[1], Some("8".to_string()));
+    }
+
+    #[test]
+    fn test3_part2() {
+        let er = ExRunner::run("day 10".to_string(), solve, test_input5());
+        er.print_raw();
+        assert_eq!(er.answ()[1], Some("10".to_string()));
     }
 }
