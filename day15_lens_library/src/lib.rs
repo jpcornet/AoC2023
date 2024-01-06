4f1e9e5c7122ddc4103f5774f4cba4c4c2c5bcc6
Jan-Pieter Cornet <jp@spamhaus.com>
2024-01-07 00:10:34 +0100

    day 15 part 2

--- day15_lens_library/src/lib.rs
+++ day15_lens_library/src/lib.rs
@@ -1,14 +1,60 @@
 use exrunner::ExRunner;
-use std::io::BufRead;
+use std::{io::BufRead, collections::HashMap};
 
 pub fn hash(s: &str) -> i32 {
     s.chars().fold(0, |acc, c| ((acc + c as i32) * 17) % 256)
+}
+
+#[derive(Debug)]
+struct LensPos {
+    focallen: i64,
+    boxpos: usize,
+}
+
+struct LensBox {
+    maxidx: usize,
+    lenses: HashMap<String, LensPos>,
 }
 
 pub fn solve(input: impl BufRead, er: &mut ExRunner) {
     let long = input.lines().map(|l| l.expect("Error reading input")).collect::<Vec<_>>().join("");
     let part1: i32 = long.split(',').map(|instr| hash(instr)).sum();
     er.part1(part1, Some("sum of HASH value of each step"));
+
+    // part2, do the lens positions. First initialize the empty boxes
+    let mut boxes = Vec::new();
+    for _ in 0..256 {
+        boxes.push(LensBox{maxidx: 0, lenses: HashMap::new()});
+    }
+    for instr in long.split(',') {
+        if let Some((name, focalstr)) = instr.split_once('=') {
+            let focallen: i64 = focalstr.parse().expect("focal length must be numeric");
+            let boxnr = hash(name);
+            let lensbox = &mut boxes[boxnr as usize];
+            lensbox.lenses.entry(name.to_string()).and_modify(|lp| lp.focallen = focallen)
+                .or_insert_with(|| {lensbox.maxidx += 1; LensPos{ focallen, boxpos: lensbox.maxidx }});
+        } else if instr.ends_with('-') {
+            let name = instr.trim_end_matches('-');
+            let boxnr = hash(name);
+            let lensbox = &mut boxes[boxnr as usize];
+            lensbox.lenses.remove(name);
+        } else {
+            panic!("Unknown instruction {}", instr);
+        }
+    }
+
+    // now calculate the total focal strength
+    let mut focalstrength = 0;
+    for boxnr in 0..boxes.len() {
+        let lensbox = &mut boxes[boxnr as usize];
+        let mut lensorder: Vec<_> = lensbox.lenses.values().collect();
+        lensorder.sort_by_key(|lp| lp.boxpos);
+        // er.debugln(&format!("Boxnr {} lenses in order: {:?}", boxnr + 1, lensorder));
+        focalstrength += lensorder.iter().enumerate()
+            .map(|(idx, lp)| (boxnr as i64 + 1) * (idx as i64 + 1) * lp.focallen)
+            .sum::<i64>();
+    }
+    er.part2(focalstrength, Some("Total focal strength after lens init"));
 }
 
 #[cfg(test)]
@@ -25,6 +71,7 @@ mod tests {
         let er = ExRunner::run("day 15".to_string(), solve, test_input());
         er.print_raw();
         assert_eq!(er.answ()[0], Some("1320".to_string()));
+        assert_eq!(er.answ()[1], Some("145".to_string()));
     }
 
     #[test]
