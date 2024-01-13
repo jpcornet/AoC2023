ab48256fbae0983bea1c006338c4b83c7bdce7d3
Jan-Pieter Cornet <jp@spamhaus.com>
2024-01-13 16:57:55 +0100

    day 16 part 2 but also test that reversing light at the output never improves things, so we can skip outputs previously seen as an optimization.

--- day16_floor_will_be_lava/src/lib.rs
+++ day16_floor_will_be_lava/src/lib.rs
@@ -1,5 +1,5 @@
 use exrunner::ExRunner;
-use std::{io::BufRead, collections::HashMap};
+use std::{io::BufRead, collections::{HashMap, HashSet}};
 
 fn parse(input: impl BufRead) -> Vec<Vec<u8>> {
     input.lines().map(|l| {
@@ -49,12 +49,22 @@ fn dxdy(dir: DIR) -> (i32, i32) {
     }
 }
 
-fn shine(floor: &Vec<Vec<u8>>, seen: &mut HashMap<(i32, i32), DIR>, x: i32, y: i32, dir: DIR) {
+fn shine(floor: &Vec<Vec<u8>>, seen: &mut HashMap<(i32, i32), DIR>, output: &mut HashSet<(i32, DIR)>, x: i32, y: i32, dir: DIR) {
     // off the grid, abort.
-    if y < 0 || y >= floor.len() as i32 {
+    if y < 0 {
+        output.insert((x, SOUTH));
         return;
     }
-    if x < 0 || x >= floor[y as usize].len() as i32 {
+    if y >= floor.len() as i32 {
+        output.insert((x, NORTH));
+        return;
+    }
+    if x < 0 {
+        output.insert((y, EAST));
+        return;
+    }
+    if x >= floor[y as usize].len() as i32 {
+        output.insert((y, WEST));
         return;
     }
 
@@ -73,38 +83,99 @@ fn shine(floor: &Vec<Vec<u8>>, seen: &mut HashMap<(i32, i32), DIR>, x: i32, y: i
     if tile == b'/' {
         let newdir = mirror1(dir);
         let (dx, dy) = dxdy(newdir);
-        shine(floor, seen, x + dx, y + dy, newdir);
+        shine(floor, seen, output, x + dx, y + dy, newdir);
         return;
     } else if tile == b'\\' {
         let newdir = mirror2(dir);
         let (dx, dy) = dxdy(newdir);
-        shine(floor, seen, x + dx, y + dy, newdir);
+        shine(floor, seen, output, x + dx, y + dy, newdir);
         return;
     } else if tile == b'-' && (dir == NORTH || dir == SOUTH) {
         // beam split east/west
-        shine(floor, seen, x + 1, y, EAST);
-        shine(floor, seen, x - 1, y, WEST);
+        shine(floor, seen, output, x + 1, y, EAST);
+        shine(floor, seen, output, x - 1, y, WEST);
         return;
     } else if tile == b'|' && (dir == EAST || dir == WEST) {
         // beam split north/south
-        shine(floor, seen, x, y - 1, NORTH);
-        shine(floor, seen, x, y + 1, SOUTH);
+        shine(floor, seen, output, x, y - 1, NORTH);
+        shine(floor, seen, output, x, y + 1, SOUTH);
         return;
     } else {
         // tile == b'.' or beam splitter in wrong direction, just continue.
         let (dx, dy) = dxdy(dir);
-        shine(floor, seen, x + dx, y + dy, dir);
+        shine(floor, seen, output, x + dx, y + dy, dir);
         return;
     }
+}
+
+fn count_energized(floor: &Vec<Vec<u8>>, x: i32, y: i32, dir: DIR) -> (usize, Vec<(i32, DIR)>) {
+    let mut light_seen = HashMap::new();
+    let mut output = HashSet::new();
+    shine(&floor, &mut light_seen, &mut output, x, y, dir);
+    (light_seen.len(), output.into_iter().collect())
 }
 
 pub fn solve(input: impl BufRead, er: &mut ExRunner) {
     let floor = parse(input);
     er.parse_done();
-    let mut light_seen = HashMap::new();
-    shine(&floor, &mut light_seen, 0, 0, EAST);
-    let energized = light_seen.keys().count();
-    er.part1(energized, Some("Number of energized tiles"));
+    // collect minimum energized level of outputs
+    let mut energy_output = HashMap::new();
+    // determine max energized level
+    let mut max_energized = None;
+    // first do all EAST and WEST light.
+    for y in 0..floor.len() {
+        for dir in [EAST, WEST] {
+            let x = if dir == EAST { 0 } else { floor[y].len() - 1 };
+            let (energized, output) = count_energized(&floor, x as i32, y as i32, dir);
+            if y == 0 && dir == EAST {
+                // part 1
+                er.part1(energized, Some("Number of energized tiles"));
+            }
+            if max_energized.is_none() || energized > max_energized.unwrap() {
+                max_energized = Some(energized);
+                println!("New max energized level {energized} shining dir {dir} pos {y}");
+            }
+            for (outpos, outdir) in output {
+                energy_output.entry((outpos, outdir)).and_modify(|lvl: &mut (usize, usize, u8)| {
+                    if lvl.0 > energized {
+                        *lvl = (energized, y, dir);
+                    }
+                }).or_insert((energized, y, dir));
+            }
+            if let Some(outlevel) = energy_output.get(&(y as i32, dir)) {
+                // test the assertion that reversing light never improves energy
+                if energized > outlevel.0 {
+                    println!("FALSE! Shining dir {} at pos {} energizes {} and gives output at dir {} pos {}, but shining into there enegizes more: {}",
+                        outlevel.2, outlevel.1, outlevel.0, dir, y, energized);
+                }
+            }
+        }
+    }
+    for x in 0..floor[0].len() {
+        for dir in [SOUTH, NORTH] {
+            let y = if dir == SOUTH { 0 } else { floor.len() - 1};
+            let (energized, output) = count_energized(&floor, x as i32, y as i32, dir);
+            if max_energized.is_none() || energized > max_energized.unwrap() {
+                max_energized = Some(energized);
+                println!("New max energized level {energized} shining dir {dir} pos {x}");
+            }
+            for (outpos, outdir) in output {
+                energy_output.entry((outpos, outdir)).and_modify(|lvl| {
+                    if lvl.0 > energized {
+                        *lvl = (energized, x, dir);
+                    }
+                }).or_insert((energized, x, dir));
+            }
+            if let Some(outlevel) = energy_output.get(&(x as i32, dir)) {
+                // test the assertion that reversing light never improves energy
+                if energized > outlevel.0 {
+                    println!("FALSE! Shining dir {} at pos {} energizes {} and gives output at dir {} pos {}, but shining into there enegizes more: {}",
+                        outlevel.2, outlevel.1, outlevel.0, dir, x, energized);
+                }
+            }
+        }
+    }
+    er.part2(max_energized.unwrap(), Some("Maximum energized tiles"));
 }
 
 #[cfg(test)]
@@ -132,5 +203,6 @@ r".|...\....
         let er = ExRunner::run("day 16".to_string(), solve, test_input());
         er.print_raw();
         assert_eq!(er.answ()[0], Some("46".to_string()));
+        assert_eq!(er.answ()[1], Some("51".to_string()));
     }
 }
